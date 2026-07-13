8d135e616e9ff329a2a23586784c60efa638099f
Pranatheesh-S <pranatheesh@gmail.com>
2026-07-13 17:49:28 +0530

    Update

---
+++ dist/assets/modifiers.esm-CS_jhdeb.js
@@ -0,0 +1,6 @@
+import{c as i}from"./index-CGaOyH1X.js";import{g as c}from"./core.esm-DQwEGk2B.js";/**
+ * @license lucide-react v1.21.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["rect",{x:"14",y:"3",width:"5",height:"18",rx:"1",key:"kaeet6"}],["rect",{x:"5",y:"3",width:"5",height:"18",rx:"1",key:"1wsw3u"}]],g=i("pause",f),u=s=>{let{activatorEvent:r,draggingNodeRect:t,transform:e}=s;if(t&&r){const o=c(r);if(!o)return e;const n=o.x-t.left,a=o.y-t.top;return{...e,x:e.x+n-t.width/2,y:e.y+a-t.height/2}}return e};export{g as P,u as s};
