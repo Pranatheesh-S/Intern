220bc56dd8d3d5c0b94366f15edfc73abdfcefb4
Pranatheesh-S <pranatheesh@gmail.com>
2026-07-23 16:30:25 +0530

    Removed .py files

--- fix.py
+++
@@ -1,8 +0,0 @@
-with open('src/science/class6/chapter6/MaterialsAroundUs/storyEngine.js', 'r') as f:
-    content = f.read()
-
-content = content.replace(']\n    dialogue:', '],\n    dialogue:')
-content = content.replace('  },,', '  },')
-
-with open('src/science/class6/chapter6/MaterialsAroundUs/storyEngine.js', 'w') as f:
-    f.write(content)
